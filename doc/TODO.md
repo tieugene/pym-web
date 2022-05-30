683d6d36ec844f22bf81be22386bbca2c10c3bf6
Eugene <ti.eugene@gmail.com>
2022-05-30 23:41:21 +0300

    TodoEntry details starts working (pseudo-AJAX)

--- doc/TODO.md
+++ doc/TODO.md
@@ -3,9 +3,22 @@
 - […] Sitemap (dot)
 - [x] Settings
 - [x] StoreList CRDL
-- [x] Store on/off (separate form)
-- [ ] Filter (separate form)
-- [ ] Sort (separate form) (note ...filtered)
+- [x] Store on/off
+- [x] Filter
+- [ ] Sort: find like quicksort using lessThen
+   *note: after filter*
+- [ ] Entry:
+  - [ ] C
+  - […] R (== **details**): radio|select | ajax
+  - [ ] U
+  - [ ] D
+  - […] L: display handlers (*Flask custom filter*)
+
+## Select entry:
+- radio -> POST -> session(entry.uid)
+- radio -> AJAX
+- onclick() = function load_entry(uid) ([RTFM](https://www.delftstack.com/howto/javascript/load-html-file-javascript/))
+- note: if uid in sessionthen can hichlitelist / clean details
 
 ## Settings:
 - store: U
@@ -17,6 +30,7 @@
 ## Issues:
 - col2show, colorder - by names
 - select Entry - invisibnle radio or select>option
+- uuid for route: https://github.com/wbolster/flask-uuid
 
 ### pym-core:
 - settings
--- pym_web/main.py
+++ pym_web/main.py
@@ -1,15 +1,31 @@
+# 1. std
+import ctypes
+# 2. 3rd
 from flask import Flask, render_template, redirect, url_for, request
 # 3. local
-import enums
 from pym_core.base.data import Store
+from pym_core.todo.data import TodoEntry
 from settings import Cfg
 from models import todo_store_model, todo_proxy_model
+import enums
 import forms
 
-app = Flask(__name__)
+app = Flask(__name__)  # type=flask.app.Flask
 app.config['SECRET_KEY'] = 'C2HWGVoMGfNTBsrYQg8EcMrdTimkZfAb'
 
 
+# template filters
+@app.template_filter()
+def prio(n):
+    ...
+
+
+@app.template_filter()
+def status(n):
+    ...
+
+
+# go
 @app.before_first_request
 def autostart():
     Cfg.setup("pym-web.json")
@@ -45,7 +61,7 @@ def todo_board():
                            )
 
 
-@app.route('/todo/add/', methods=['GET', 'POST'])
+@app.route('/todo/store/add/', methods=['GET', 'POST'])
 def todo_store_add():
     # Sample: ti.Cloud - /Users/eugene/VCS/my/GIT/pyqtpim/_data/eap/ti.Cloud
     form = forms.StoreForm()
@@ -58,48 +74,58 @@ def todo_store_add():
     return render_template('store_form.html', form=form)
 
 
-@app.route('/todo/del/<int:store>/', methods=['GET'])
+@app.route('/todo/store/del/<int:store>/', methods=['GET'])
 def todo_store_del(store: int):
     todo_store_model.item_del(store)
     return redirect(url_for('todo_board'))
 
 
-@app.route('/todo/sel/', methods=['POST'])
+@app.route('/todo/store/sel/', methods=['POST'])
 def todo_store_sel():
     """Switch store on/off"""
-    if request.method == 'POST':
-        # getlist(): list of checked checkboxes value:str
-        checked_set = set([int(i) for i in request.form.getlist('store')])
-        print("Checked:", checked_set)
-        for i, store in enumerate(todo_store_model.items()):
-            checked = i in checked_set
-            if store.active != checked:
-                store.active = checked
-                todo_store_model.save_self()
-                break  # just 1 click per request
+    # getlist(): list of checked checkboxes value:str
+    checked_set = set([int(i) for i in request.form.getlist('store')])
+    print("Checked:", checked_set)
+    for i, store in enumerate(todo_store_model.items()):
+        checked = i in checked_set
+        if store.active != checked:
+            store.active = checked
+            todo_store_model.save_self()
+            break  # just 1 click per request
     return redirect(url_for('todo_board'))
 
 
 @app.route('/todo/sort/', methods=['POST'])
 def todo_set_sort():
     """Set entries sort order"""
-    if request.method == 'POST':
-        print("Sort:", request.form.get('sort'))
-        # set proxy sort
-        # save cfg
+    print("Sort:", request.form.get('sort'))
+    # set proxy sort
+    # save cfg
     return redirect(url_for('todo_board'))
 
 
 @app.route('/todo/filt/', methods=['POST'])
 def todo_set_filt():
     """Set entries filter"""
-    if request.method == 'POST':
-        # print("Filt:", request.form.get('filt'))
-        # set proxy filter
-        filt = int(request.form.get('filt'))
-        todo_proxy_model.switchFilter(filt)
-        Cfg.set(enums.SetGroup.ToDo, 'filt', filt)
+    # print("Filt:", request.form.get('filt'))
+    # set proxy filter
+    filt = int(request.form.get('filt'))
+    todo_proxy_model.switchFilter(filt)
+    Cfg.set(enums.SetGroup.ToDo, 'filt', filt)
     return redirect(url_for('todo_board'))
+
+
+@app.route('/todo/entry/view/', methods=['POST'])
+def todo_entry_view():
+    return redirect(url_for('todo_board'))
+
+
+@app.route('/todo/entry/detail/', methods=['GET'])
+def todo_entry_detail():
+    """Powered by https://thewebdev.info/2022/04/10/how-to-get-object-by-id-with-python/"""
+    oid = int(request.args.get('id'))
+    entry: TodoEntry = ctypes.cast(oid, ctypes.py_object).value
+    return render_template("todo_entry_details.html", entry=entry)
 
 
 if __name__ == '__main__':
